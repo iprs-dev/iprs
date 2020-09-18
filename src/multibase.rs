f816e0caff650af965368965837050dcc63bf733
prataprc <prataprc@gmail.com>
2020-09-18 21:32:06 +0530

    multibase: refactor and test cases.

--- src/multibase.rs
+++ src/multibase.rs
@@ -2,12 +2,11 @@
 //!
 //! [multibase]: https://github.com/multiformats/multibase
 
+use multibase;
+
 use std::io;
 
-use crate::{
-    multicodec::{self, Multicodec},
-    Error, Result,
-};
+use crate::{Error, Result};
 
 /// Type to encode/decode bytes into/from multi-base formats.
 ///
@@ -15,7 +14,6 @@ use crate::{
 ///
 /// [multibase]: https://github.com/multiformats/multibase
 pub struct Multibase {
-    codec: Multicodec,
     base: multibase::Base,
     data: Option<Vec<u8>>,
 }
@@ -23,12 +21,7 @@ pub struct Multibase {
 impl Multibase {
     /// Create a multibase encoder from one of the many base formats.
     pub fn from_base(base: multibase::Base) -> Result<Multibase> {
-        let val = Multibase {
-            codec: multicodec::MULTIBASE.into(),
-            base,
-            data: None,
-        };
-        Ok(val)
+        Ok(Multibase { base, data: None })
     }
 
     /// Create a multibase encoder from character prefix defined in multibase
@@ -36,31 +29,33 @@ impl Multibase {
     ///
     /// [specification]: https://github.com/multiformats/multibase/blob/master/multibase.csv
     pub fn from_char(ch: char) -> Result<Multibase> {
-        let val = Multibase {
-            codec: multicodec::MULTIBASE.into(),
-            base: err_at!(Invalid, multibase::Base::from_code(ch))?,
-            data: None,
-        };
-        Ok(val)
+        let base = match multibase::Base::from_code(ch) {
+            Ok(base) => Ok(base),
+            Err(e) => err_at!(BadInput, Err(e), format!("bad char `{}`", ch)),
+        }?;
+
+        Ok(Multibase { base, data: None })
     }
 
-    /// Decode base-format into binary-data.
-    ///
-    /// Use the returned `Multibase` type to get the binary-data.
-    ///
-    /// ```ignore
-    ///     Multibase::from_slice(input)?.to_bytes()
-    /// ```
-    pub fn from_slice(buf: &[u8]) -> Result<Multibase> {
+    /// Encode input binary-data using this base format, encoded stream of
+    /// bytes shall have the <base-prefix> followed by the actual
+    /// base-representation of the `input`.
+    pub fn encode<I: AsRef<[u8]>>(&self, input: I) -> Result<Vec<u8>> {
+        let mut buf = Vec::default();
+        self.encode_with(input, &mut buf)?;
+        Ok(buf)
+    }
+
+    /// Decode <base-prefix> followed by the base-representation, into
+    /// raw-data. Caller can use the returned value to get the base
+    /// format and the original raw-data.
+    pub fn decode(buf: &[u8]) -> Result<Multibase> {
         use std::str::from_utf8;
 
-        let (codec, rem) = Multicodec::from_slice(buf)?;
-
-        let s = err_at!(BadInput, from_utf8(rem))?;
-        let (base, data) = err_at!(Invalid, multibase::decode(s))?;
+        let text = err_at!(BadInput, from_utf8(buf))?;
+        let (base, data) = err_at!(BadInput, multibase::decode(text))?;
 
         let val = Multibase {
-            codec,
             base,
             data: Some(data),
         };
@@ -68,32 +63,16 @@ impl Multibase {
         Ok(val)
     }
 
-    /// Encode input binary-data using this base format.
-    pub fn encode<I: AsRef<[u8]>>(&self, input: I) -> Result<Vec<u8>> {
-        let mut buf = Vec::default();
-        self.encode_with(input, &mut buf)?;
-        Ok(buf)
-    }
-
-    /// Same as encode but avoids memory allocation by using the supplied
-    /// buffer `buf`.
-    pub fn encode_with<I, W>(&self, input: I, buf: &mut W) -> Result<usize>
+    // Same as encode but avoids memory allocation by using the supplied
+    // buffer `buf`.
+    fn encode_with<I, W>(&self, input: I, buf: &mut W) -> Result<usize>
     where
         I: AsRef<[u8]>,
         W: io::Write,
     {
-        let n = self.codec.encode_with(buf)?;
-
         let text = multibase::encode(self.base.clone(), input);
         err_at!(IOError, buf.write(text.as_bytes()))?;
-        let m = text.len();
-
-        Ok(n + m)
-    }
-
-    /// Return the codec value.
-    pub fn to_codec(&self) -> Multicodec {
-        self.codec.clone()
+        Ok(text.len())
     }
 
     /// Return the `Base` format type.
@@ -101,14 +80,70 @@ impl Multibase {
         self.base.clone()
     }
 
-    /// Return the decoded binary-data from base-format.
+    /// Return the decoded original binary-data from base-format.
     pub fn to_bytes(&self) -> Option<Vec<u8>> {
         self.data.clone()
     }
-
-    /// Same as to_bytes() but return a reference, might be cheaper
-    /// than calling `to_bytes()`.
-    pub fn as_bytes(&self) -> Option<&[u8]> {
-        self.data.as_ref().map(|x| x.as_slice())
-    }
 }
+
+pub const TABLE: [(&'static str, char, &'static str); 23] = [
+    (
+        "identity",
+        '\0',
+        "8-bit binary (encoder and decoder keeps data unmodified)",
+    ),
+    ("base2", '0', "binary (01010101)"),
+    ("base8", '7', "octal"),
+    ("base10", '9', "decimal"),
+    ("base16", 'f', "hexadecimal"),
+    ("base16upper", 'F', "hexadecimal"),
+    (
+        "base32hex",
+        'v',
+        "rfc4648 case-insensitive - no padding - highest char",
+    ),
+    (
+        "base32hexupper",
+        'V',
+        "rfc4648 case-insensitive - no padding - highest char",
+    ),
+    (
+        "base32hexpad",
+        't',
+        "rfc4648 case-insensitive - with padding",
+    ),
+    (
+        "base32hexpadupper",
+        'T',
+        "rfc4648 case-insensitive - with padding",
+    ),
+    ("base32", 'b', "rfc4648 case-insensitive - no padding"),
+    ("base32upper", 'B', "rfc4648 case-insensitive - no padding"),
+    ("base32pad", 'c', "rfc4648 case-insensitive - with padding"),
+    (
+        "base32padupper",
+        'C',
+        "rfc4648 case-insensitive - with padding",
+    ),
+    ("base32z", 'h', "z-base-32 (used by Tahoe-LAFS)"),
+    (
+        "base36",
+        'k',
+        "base36 [0-9a-z] case-insensitive - no padding",
+    ),
+    (
+        "base36upper",
+        'K',
+        "base36 [0-9a-z] case-insensitive - no padding",
+    ),
+    ("base58btc", 'z', "base58 bitcoin"),
+    ("base58flickr", 'Z', "base58 flicker"),
+    ("base64", 'm', "rfc4648 no padding"),
+    ("base64pad", 'M', "rfc4648 with padding - MIME encoding"),
+    ("base64url", 'u', "rfc4648 no padding"),
+    ("base64urlpad", 'U', "rfc4648 with padding"),
+];
+
+#[cfg(test)]
+#[path = "multibase_test.rs"]
+mod multibase_test;
---
+++ src/multibase_test.rs
@@ -0,0 +1,73 @@
+use super::*;
+use reqwest::blocking::get;
+
+#[test]
+fn test_base_spec() {
+    let spec = {
+        let uri = "https://raw.githubusercontent.com/multiformats/multibase/master/multibase.csv";
+        get(uri).unwrap().text().unwrap()
+    };
+
+    let spec_lines: Vec<(String, char, String)> = {
+        let mut total_lines: Vec<String> = {
+            let iter = spec.lines().map(|s| s.to_string());
+            iter.collect()
+        };
+        total_lines.remove(0); // remove the column header.
+        total_lines
+            .into_iter()
+            .map(|s| {
+                let mut cols: Vec<&str> = s.split(",").map(|col| col.trim()).collect();
+                cols.pop();
+                match cols.as_slice() {
+                    [n, "0x00", d] => (n.to_string(), '\0', d.to_string()),
+                    [n, ch, d] => {
+                        let ch = ch.chars().nth(0).unwrap();
+                        (n.to_string(), ch, d.to_string())
+                    }
+                    _ => panic!("{:?}", cols),
+                }
+            })
+            .collect()
+    };
+
+    let pkg_lines: Vec<(String, char, String)> = (&TABLE)
+        .to_vec()
+        .into_iter()
+        .map(|(name, ch, descr)| (name.to_string(), ch, descr.to_string()))
+        .collect();
+
+    assert_eq!(
+        spec_lines.len(),
+        pkg_lines.len(),
+        "{} {}",
+        spec_lines.len(),
+        pkg_lines.len()
+    );
+
+    for (x, y) in spec_lines.into_iter().zip(pkg_lines.into_iter()) {
+        assert_eq!(x.0, y.0, "{:?}, {:?}", x, y);
+        assert_eq!(x.1, y.1, "{:?}, {:?}", x, y);
+        assert_eq!(x.2, y.2, "{:?}, {:?}", x, y);
+    }
+}
+
+#[test]
+fn test_base_formats() {
+    use std::str::from_utf8;
+
+    for row in TABLE.iter() {
+        match row.1 {
+            'k' | 'K' => continue,
+            _ => (),
+        }
+
+        let mb = Multibase::from_char(row.1).unwrap();
+        let out = mb.encode("hello world".as_bytes()).unwrap();
+        println!("{:?} {}", row.1, from_utf8(&out).unwrap());
+
+        let data = Multibase::decode(&out).unwrap().to_bytes().unwrap();
+        let text = from_utf8(&data).unwrap();
+        assert_eq!(text, "hello world");
+    }
+}
