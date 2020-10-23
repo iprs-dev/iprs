8783366669e498938bc33129ab56efad298b3328
prataprc <prataprc@gmail.com>
2020-10-23 22:35:09 +0530

    schema: dev commit.

--- src/ipld/schema.rs
+++ src/ipld/schema.rs
@@ -1,28 +1,39 @@
 struct Schema {
     name: String,
     typ: Type,
+    mods: Vec<Modifier>,
     repr: Representation,
 }
 
 enum Type {
-    Kind,
-    Struct(Struct),
-    Union,
-    Enum,
+    Null,
+    Bool,
+    Integer,
+    Float,
+    Text,
+    Bytes,
+    List,  // only homogenous value in list
+    Map,   // only string keys.
+    Union, // as map or varying data-mode-kinds for kinded union
+    Tuple, // as map in data-model
+    Enum,  // either as string or int.
+    Copyy,
 }
 
 struct Struct {
     elements: Vec<Element>,
 }
 
-struct Element {
-    name: String,
-    nullable: bool,
-    optional: bool,
+enum Modifier {
+    Nullable,  // for map-value, list-value and struct fields
+    Optional,  // for struct-fields
+    Implicite, // for struct-fields
 }
 
 enum Representation {
     Keyed,
+    Envelope,
+    Inline,
 }
 
 // Examples
